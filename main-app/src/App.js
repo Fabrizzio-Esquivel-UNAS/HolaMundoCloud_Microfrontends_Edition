3976ab7167cc15ec0febfdc372701e140696f3e0
Fabrizzio Fabiano Esquivel Mori <fabrizzio.esquivel@unas.edu.pe>
2024-12-20 07:45:06 -0500

    host 4

--- main-app/src/App.js
+++ main-app/src/App.js
@@ -17,7 +17,7 @@ function App() {
     setSubAppsLoaded(true);
     loadMicroApp({
         name: 'sub1 app',
-        entry: 'http://3.15.6.49:3001',
+        entry: 'http://3.144.8.160',
         container: subApp1Ref.current,
         props: { title: 'Sub App 1' },
       });
