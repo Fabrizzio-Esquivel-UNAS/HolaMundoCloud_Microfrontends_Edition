fc06b9bebeab32e0404320b705db26dabc099668
Fabrizzio Fabiano Esquivel Mori <fabrizzio.esquivel@unas.edu.pe>
2024-12-20 07:12:13 -0500

    host 1

--- sub-app/public/index.js
+++ sub-app/public/index.js
@@ -4,7 +4,7 @@ if (document.getElementById('fetch-message')){
     document.getElementById('fetch-message').addEventListener('click', function() {
         // Call the API when the button is clicked
         console.log("Clicked")
-        fetch('http://localhost:3030/message') // Replace with your actual API endpoint
+        fetch('http://3.15.6.49:3030/message') // Replace with your actual API endpoint
             .then(response => response.json()) // Assuming the response is in JSON format
             .then(data => {
             // Display the message in the <p> element with id="message"
